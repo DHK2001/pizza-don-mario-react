9399b9b10edaab75e8204f837173de874d3bc70e
Derek Galeas <dhenrygk12@gmail.com>
2025-01-08 12:03:49 -0600

    pathname navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,56 +2,29 @@
 
 import React from 'react'
 import Link from 'next/link';
-import { useState } from "react";
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
-  const [home, setHome] = useState(true);
-  const [menu, setMenu] = useState(false);
-  const [about, setAbout] = useState(false);
-  const [contact, setContact] = useState(false);  
+  const pathname = usePathname();
 
   return (
     <>
         <nav className="nav-bar">
           <ul>
               <li 
-              className={home ? "nav-element-selected" : "nav-element"} 
-              onClick={() => {
-                setHome(true);
-                setMenu(false);
-                setAbout(false);
-                setContact(false);
-              }}>
+              className={pathname == "/" ? "nav-element-selected" : "nav-element"}>
                 <Link href="/">Home</Link>
               </li>
               <li 
-              className={menu ? "nav-element-selected" : "nav-element"}
-              onClick={() => {
-                setHome(false);
-                setMenu(true);
-                setAbout(false);
-                setContact(false);
-              }}>
+              className={pathname == "/menu"  ? "nav-element-selected" : "nav-element"}>
                 <Link href="/menu">Menu</Link>
               </li>
               <li 
-              className={about ? "nav-element-selected" : "nav-element"}
-              onClick={() => {
-                setHome(false);
-                setMenu(false);
-                setAbout(true);
-                setContact(false);
-              }}>
+              className={pathname == "/about"  ? "nav-element-selected" : "nav-element"}>
                 <Link href="/about">About Us</Link>
               </li>
               <li 
-              className={contact ? "nav-element-selected" : "nav-element"}
-              onClick={() => {
-                setHome(false);
-                setMenu(false);
-                setAbout(false);
-                setContact(true);
-              }}>
+              className={pathname == "/contact"  ? "nav-element-selected" : "nav-element"}>
                 <Link href="/contact">Contact Us</Link>
               </li>
           </ul>
